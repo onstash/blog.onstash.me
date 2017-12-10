dcb172f5b283b90dbf89f97d13a852085f2c9522
Santosh Venkatraman <santosh.venk@gmail.com>
2017-12-10 20:49:39 +0530

    Adds open graph & twitter card meta tags

---
+++ src/components/metadata/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Helmet from 'react-helmet';
+
+const Metadata = ({ description, title }) => (
+    <Helmet>
+        <meta name="description" content={description} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:type" content="website" />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:title" content={title} />
+        <meta property="twitter:description" content={description} />
+        <meta property="twitter:site" content="@on_stash" />
+        <meta property="twitter:creator" content="@on_stash" />
+    </Helmet>
+);
+
+export default Metadata;
